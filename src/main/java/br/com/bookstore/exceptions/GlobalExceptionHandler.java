a80f3c598cbb8d9f0d718477f90659aabeca688a
unknown <rn.sgnaolin@gmail.com>
2025-11-21 20:51:21 -0300

    Changed handling structure to refer to http status thrown

--- src/main/java/br/com/bookstore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/bookstore/exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,35 @@
 package br.com.bookstore.exceptions;
 
+import java.net.BindException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.persistence.EntityNotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> entityNotFound() {
+    public ResponseEntity<ErrorResponse> handleNotFound() {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
             .body(new ErrorResponse());
     }
+
+    @ExceptionHandler({
+        BindException.class,
+        MethodArgumentNotValidException.class,
+        IllegalArgumentException.class,
+        HttpMessageNotReadableException.class
+})
+    public ResponseEntity<ErrorResponse> handleBadRequest(Exception ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(ex.getMessage()));
+    }
+
+
 }
