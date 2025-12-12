4153b7c988380d2d02ea76cc65faf21acb7abeb8
unknown <rn.sgnaolin@gmail.com>
2025-12-12 01:05:43 -0300

    Added fetch type

--- src/main/java/br/com/bookstore/entity/Book.java
+++ src/main/java/br/com/bookstore/entity/Book.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -45,7 +46,7 @@ public class Book {
     private int stock;
     private boolean deleted;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id")
     private Author author;
 }
