001c1fcaa01a6ec777f9a8f61d1cba3ab8a6ca5d
unknown <rn.sgnaolin@gmail.com>
2025-12-17 20:58:34 -0300

    Add @Positive for relevant fields to avoid relying on DB constraints

--- src/main/java/br/com/bookstore/dto/BookCreateDTO.java
+++ src/main/java/br/com/bookstore/dto/BookCreateDTO.java
@@ -5,18 +5,20 @@ import java.math.BigDecimal;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record BookCreateDTO(
     @NotBlank(message = "Título não pode estar vazio") 
     @Schema(example = "A Game of Thrones")
     String title, 
     @NotNull(message = "Preço inválido") 
+    @Positive(message = "Preço deve ser maior que zero")
     @Schema(example = "59.99")
     BigDecimal price, 
     @NotNull(message = "ID do Autor não pode estar vazio") 
     @Schema(example = "1")
     Long authorId, 
-    @NotNull(message = "Número de páginas não pode ser vazio") 
+    @Positive(message = "Número de páginas deve ser maior que zero")
     @Schema(example = "720")
     int pageCount) {
 }
--- src/main/java/br/com/bookstore/dto/BookUpdateDTO.java
+++ src/main/java/br/com/bookstore/dto/BookUpdateDTO.java
@@ -3,14 +3,17 @@ package br.com.bookstore.dto;
 import java.math.BigDecimal;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Positive;
 
 public record BookUpdateDTO(
     @Schema(example = "A Clash of Kings")
     String title, 
+    @Positive(message = "Preço deve ser maior que zero")
     @Schema(example = "72.99")
     BigDecimal price, 
     @Schema(example = "1")
     Long authorId, 
+    @Positive(message = "Preço deve ser maior que zero")
     @Schema(example = "761")
     Integer pageCount) {
     
