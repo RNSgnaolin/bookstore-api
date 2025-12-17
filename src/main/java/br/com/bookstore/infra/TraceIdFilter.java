6a7e000226787ccaf152c49ffb031b68439375aa
unknown <rn.sgnaolin@gmail.com>
2025-12-17 18:55:22 -0300

    Add trace ID generation for future logging and responses

---
+++ src/main/java/br/com/bookstore/infra/TraceIdFilter.java
@@ -0,0 +1,35 @@
+package br.com.bookstore.infra;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class TraceIdFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain
+    ) throws IOException, ServletException {
+
+        String traceId = UUID.randomUUID().toString();
+
+        try {
+            MDC.put("traceId", traceId);
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(traceId);
+        }
+
+    }
+    
+}
